f9162377f7f95b2fd084e9338d3af2481cb79995
Franck DAKIA <dakiafranck@gmail.com>
2023-03-14 09:18:36 +0000

    Add destination and source

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,16 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
+if [ -z "${FROM}" ]; then
+   FROM='s3'
+fi
+
+if [ "${FROM}" = 'local' ];
+   DESTINATION="s3://${AWS_S3_BUCKET}${DESTINATION}"
+else:
+   SOURCE="s3://${AWS_S3_BUCKET}${SOURCE}"
+if
+
 aws s3 cp ${SOURCE} ${DESTINATION} \
             --region ${AWS_REGION} $*
 
